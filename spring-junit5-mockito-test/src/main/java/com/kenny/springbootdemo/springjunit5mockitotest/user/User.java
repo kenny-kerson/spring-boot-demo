47a37934b818ab9dc5d4246e1521bfe012e5cee0
kenny.k <kenny.kerson@gmail.com>
2020-10-01 21:29:19 +0900

    1.JUnit5 : Assertion & AssertJ

--- spring-junit5-mockito-test/src/main/java/com/kenny/springbootdemo/springjunit5mockitotest/user/User.java
+++ spring-junit5-mockito-test/src/main/java/com/kenny/springbootdemo/springjunit5mockitotest/user/User.java
@@ -14,4 +14,5 @@ public class User {
     private String username;
     private String password;
     private String roles;
+    private Boolean isMale;
 }
--- spring-junit5-mockito-test/src/test/java/com/kenny/springbootdemo/springjunit5mockitotest/user/UserTest.java
+++ spring-junit5-mockito-test/src/test/java/com/kenny/springbootdemo/springjunit5mockitotest/user/UserTest.java
@@ -1,9 +1,9 @@
 package com.kenny.springbootdemo.springjunit5mockitotest.user;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class UserTest {
 
@@ -18,18 +18,112 @@ class UserTest {
     }
 
     @Test
-    @DisplayName("유저 만들기1")
+    @DisplayName("유저 만들기1 😀")
     void createUser1() {
         User user = new User();
         assertThat(user).isNotNull();
     }
 
     @Test
-    @DisplayName("유저 만들기2")
+    @DisplayName("유저 만들기2 🥰")
     @Disabled
     void createUser2() {
         User user = new User();
         assertThat(user).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Assertion : Null 체크")
+    void assertionNullCheck() {
+        final User user = null;
+
+        assertThat(user).isNull();
+    }
+
+    @Test
+    @DisplayName("Assertion : NotNull 체크")
+    void assertionNotNullCheck() {
+        final User user = User.builder()
+                .id(1)
+                .build();
+
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Assertion : True or False 체크")
+    void assertionTrueOrFalseCheck() {
+        final User user = User.builder()
+                .id(1)
+                .isMale(Boolean.TRUE)
+                .build();
+
+        assertThat(user.getIsMale())
+                .as("True is Male")
+                .isTrue();
+
+    }
+
+    @Test
+    @DisplayName("Assertion : 동일여부 체크")
+    void assertionEqualsCheck() {
+        final User user = User.builder()
+                .id(1)
+                .build();
+
+        assertThat(user.getId()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Assertion : 모든 Assertion 체크")
+    void assertionAllCheck() {
+        final String role = "USER";
+        final String username = "kenny";
+
+        final User user = User.builder()
+                .id(1)
+                .username(username)
+                .password("1234")
+                .roles(role)
+                .isMale(Boolean.TRUE)
+                .build();
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThat(user.getId()).isEqualTo(1);
+        softAssertions.assertThat(user.getUsername()).isEqualTo(username);
+        softAssertions.assertThat(user.getIsMale()).isTrue();
+        softAssertions.assertThat(user.getRoles()).isEqualTo(role);
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @DisplayName("Assertion : NullPointException 체크")
+    void assertionNPECheck() {
+        final User user = User.builder()
+                .id(1)
+                .build();
+
+        assertThatNullPointerException()
+                .isThrownBy(() -> user.getUsername().substring(0,3));
+    }
+
+    @Test
+    @DisplayName("Assertion : Exception 체크")
+    void assertionExceptionCheck() {
+        final User user = User.builder()
+                .id(1)
+                .build();
+
+        assertThatThrownBy(() -> user.getUsername().substring(0, 3))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("Assertion : Timeout 체크")
+    void assertionTimeoutCheck() {
+        // assertj에는 Timeout 체크기능을 별도로 없는듯?!
     }
 
     @AfterEach
