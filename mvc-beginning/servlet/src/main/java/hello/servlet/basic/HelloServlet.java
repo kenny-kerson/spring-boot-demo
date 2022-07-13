b01e4ca7c88424d9673f0707b825b62d54d51f27
kenny <kenny.kerson@gmail.com>
2022-07-13 19:01:21 +0900

    # 서블릿 name 변경

--- mvc-beginning/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ mvc-beginning/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "helloSerlvet", urlPatterns = "/hello")
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
