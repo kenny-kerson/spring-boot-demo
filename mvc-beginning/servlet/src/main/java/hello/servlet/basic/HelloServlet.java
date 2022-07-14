085933f4e7040ebb2f929c8a5074be28441c9c0c
kenny <kenny.kerson@gmail.com>
2022-07-14 09:38:24 +0900

    # Servlet 코드에 주석 추가

--- mvc-beginning/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ mvc-beginning/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -17,11 +17,15 @@ public class HelloServlet extends HttpServlet {
         System.out.println("request = " + request);
         System.out.println("response = " + response);
 
+        // HTTP 요청의 쿼리스트링 GET
         final String username = request.getParameter("username");
         System.out.println("username = " + username);
 
+        // HTTP Header 셋팅
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
+
+        // HTTP Body 셋팅
         response.getWriter().write("hello " + username);
     }
 }
