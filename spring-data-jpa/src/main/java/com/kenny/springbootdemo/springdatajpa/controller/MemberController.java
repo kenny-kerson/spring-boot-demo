39a8e88f552d3cc62084ec7607f7a77642f51c92
kenny.k <kenny.k@kakaobank.com>
2022-09-08 11:24:21 +0900

    # sprint-data-jpa 하위모듈 최초 추가2

--- spring-data-jpa/src/main/java/com/kenny/springbootdemo/springdatajpa/controller/MemberController.java
+++ spring-data-jpa/src/main/java/com/kenny/springbootdemo/springdatajpa/controller/MemberController.java
@@ -1,10 +1,6 @@
 package com.kenny.springbootdemo.springdatajpa.controller;
 
-import com.kenny.springbootdemo.springdatajpa.domain.Member;
-import com.kenny.springbootdemo.springdatajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,29 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
-    private final MemberRepository memberRepository;
 
     @PostMapping("/member")
     @Transactional
     public void registerMember() {
-        registerMember1();
-        memberService.registerMember2();
+        memberService.registerMember();
     }
-
-    @Transactional
-    public void registerMember1() {
-        Member member1 = Member.builder()
-                .id(1L)
-                .name("홍길동")
-                .build();
-
-        memberRepository.save(member1);
-    }
-
 
     @GetMapping("/member")
     public void inquiryMember() {
-        memberRepository.findById(1L);
-        memberRepository.findById(2L);
+        memberService.inquiryMember();
     }
 }
--- spring-data-jpa/src/main/java/com/kenny/springbootdemo/springdatajpa/controller/MemberServiceRequiresNew.java
+++ spring-data-jpa/src/main/java/com/kenny/springbootdemo/springdatajpa/controller/MemberServiceRequiresNew.java
@@ -4,29 +4,24 @@ import com.kenny.springbootdemo.springdatajpa.domain.Member;
 import com.kenny.springbootdemo.springdatajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberServiceRequiresNew {
 
-    private final MemberServiceRequiresNew memberServiceRequiresNew;
     private final MemberRepository memberRepository;
 
-
-    @Transactional
-    public void registerMember() {
-        Member member1 = Member.builder()
-                .id(1L)
-                .name("홍길동")
+    //    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.DEFAULT)
+    public void registerMember2() {
+        Member member2 = Member.builder()
+                .id(2L)
+                .name("고길동")
                 .build();
 
-        memberRepository.save(member1);
-        memberServiceRequiresNew.registerMember2();
-    }
-
-    public void inquiryMember() {
-        memberRepository.findById(1L);
-        memberRepository.findById(2L);
+        memberRepository.save(member2);
     }
 }
