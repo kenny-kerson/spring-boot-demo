12207154a06a947e0013461aa763d9fa93d7c2ba
kenny.k <kenny.k@kakaobank.com>
2023-03-09 17:26:58 +0900

    feat: [test-was] MySQL에서 SEQUENCE 테스트 #1
    - 테스트를 위한 기본 클래스 및 설정 추가

--- test-was/src/main/java/com/kenny/testwas/domain/User.java
+++ test-was/src/main/java/com/kenny/testwas/domain/User.java
@@ -1,4 +1,14 @@
 package com.kenny.testwas.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
 }
