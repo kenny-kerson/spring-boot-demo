3c7864d0bec72e6ba0da58c65b0e4044ba17c2ac
kenny-kerson <kenny.kerson@gmail.com>
2023-03-20 23:20:27 +0900

    docs: [gs-caching] 주석추가

--- getting-started-guides/gs-caching/src/main/java/com/kenny/gscaching/GsCachingApplication.java
+++ getting-started-guides/gs-caching/src/main/java/com/kenny/gscaching/GsCachingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+// 캐싱기능을 활성화 시킨다 : @EnableCaching
 public class GsCachingApplication {
 
     public static void main(String[] args) {
--- getting-started-guides/gs-caching/src/main/java/com/kenny/gscaching/SimpleBookRepository.java
+++ getting-started-guides/gs-caching/src/main/java/com/kenny/gscaching/SimpleBookRepository.java
@@ -8,6 +8,7 @@ public class SimpleBookRepository implements BookRepository {
 
     @Override
     @Cacheable("books")
+    // cache 라이브러리가 의존성에 없을때, @Cacheable을 사용하면, ConcurrentHashMap을 사용한다.
     public Book getByIsbn(final String isbn) {
         simulateSlowService();
         return new Book(isbn, "Some book");
