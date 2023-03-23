5715ae5de49d5425c9d6d94efd03f9a503324677
kenny-kerson <kenny.kerson@gmail.com>
2023-03-23 22:00:51 +0900

    feat: [gs-rest-service] Building a RESTful Web Service 예제 클론코딩

---
+++ getting-started-guides/gs-rest-service/src/test/java/com/kenny/gsrestservice/GsRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.gsrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
