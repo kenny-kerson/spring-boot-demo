c62710edcb469b7dac2b68753e935f499af6a577
kenny-kerson <kenny.kerson@gmail.com>
2023-03-29 23:18:55 +0900

    feat: [gs-spring-data-reactive-redis] Accessing Data Reactivly with Redis 예제 클론코딩 #2ˆ

--- getting-started-guides/gs-spring-data-reactive-redis/src/main/java/com/kenny/gsspringdatareactiveredis/ui/CoffeeController.java
+++ getting-started-guides/gs-spring-data-reactive-redis/src/main/java/com/kenny/gsspringdatareactiveredis/ui/CoffeeController.java
@@ -1,4 +1,23 @@
 package com.kenny.gsspringdatareactiveredis.ui;
 
+import com.kenny.gsspringdatareactiveredis.domain.Coffee;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
 public class CoffeeController {
+
+    private final ReactiveRedisOperations<String, Coffee> coffeeOps;
+
+    public CoffeeController(final ReactiveRedisOperations<String, Coffee> coffeeOps) {
+        this.coffeeOps = coffeeOps;
+    }
+
+    @GetMapping("/coffees")
+    public Flux<Coffee> all() {
+        return coffeeOps.keys("*")
+                .flatMap(coffeeOps.opsForValue()::get);
+    }
 }
