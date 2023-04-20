46db027e2059cf5e101939e0950b19c4e05f2fed
kenny-kerson <kenny.kerson@gmail.com>
2023-04-20 19:04:33 +0900

    feat: [spring-webflux][learning-test] WebClient를 이용한 cold / hot sequence 테스트코드 작성

--- spring-webflux/src/test/java/com/kenny/springwebflux/learning/hotandcold/ColdSequenceTest.java
+++ spring-webflux/src/test/java/com/kenny/springwebflux/learning/hotandcold/ColdSequenceTest.java
@@ -1,20 +1,32 @@
 package com.kenny.springwebflux.learning.hotandcold;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.util.Arrays;
 
+@ExtendWith(SpringExtension.class)
+@Slf4j
 public class ColdSequenceTest {
 
     @Test
     void Cold_Sequence_Basic() {
-        final Flux<String> coldFlux = Flux.fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE"))
-                .doOnNext(country -> System.out.println("# doOnNext : " + country))
-                .map(String::toLowerCase)
-                .doOnNext(country -> System.out.println("# map : " + country));
 
-        coldFlux.subscribe(country -> System.out.println("# subscribe1 : " + country));
+        final Flux<String> coldFlux = Flux.fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE"))
+                .doOnNext(country -> log.warn("# doOnNext : " + country))
+                .map(String::toLowerCase)
+                .doOnNext(country -> log.warn("# map : " + country));
+
+        coldFlux.subscribe(country -> log.warn("# subscribe1 : " + country));
 
         try {
             Thread.sleep(2000L);
@@ -22,6 +34,52 @@ public class ColdSequenceTest {
 
         }
 
-        coldFlux.subscribe(country -> System.out.println("# subscribe2 : " + country));
+        coldFlux.subscribe(country -> log.warn("# subscribe2 : " + country));
+
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    @Test
+    void HTTP_Request_Cold_Sequence() {
+        final URI uri = UriComponentsBuilder.newInstance().scheme("http")
+                .host("worldtimeapi.org")
+                .port(80)
+                .path("/api/timezone/Asia/Seoul")
+                .build()
+                .encode()
+                .toUri();
+
+        final Mono<String> mono = getWorldTime(uri);
+        mono.subscribe(dateTime -> log.info("# dateTime 1: {}", dateTime));
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        mono.subscribe(dateTime -> log.info("# dateTime 2: {}", dateTime));
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Mono<String> getWorldTime(final URI uri) {
+        return WebClient.create()
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(response -> {
+                    final DocumentContext jsonContext = JsonPath.parse(response);
+                    return jsonContext.read("$.datetime");
+                });
     }
 }
--- spring-webflux/src/test/java/com/kenny/springwebflux/learning/hotandcold/HotSequenceTest.java
+++ spring-webflux/src/test/java/com/kenny/springwebflux/learning/hotandcold/HotSequenceTest.java
@@ -1,21 +1,32 @@
 package com.kenny.springwebflux.learning.hotandcold;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.time.Duration;
 import java.util.Arrays;
 
+@ExtendWith(SpringExtension.class)
+@Slf4j
 public class HotSequenceTest {
 
     @Test
     void Hot_Sequence_Basic() {
         final Flux<String> hotFlux = Flux.fromIterable(Arrays.asList("Singer A", "Singer B", "Singer C", "Singer D", "Singer E"))
                 .delayElements(Duration.ofSeconds(1))
-                .doOnNext(singer -> System.out.println("# doOnNext : " + singer + " Thread : " + Thread.currentThread()))
+                .doOnNext(singer -> log.warn("# doOnNext : " + singer))
                 .share();
 
-        hotFlux.subscribe(singer -> System.out.println("# subscribe1 : " + singer + " Thread : " + Thread.currentThread()));
+        hotFlux.subscribe(singer -> log.warn(("# subscribe1 : " + singer)));
 
         try {
             Thread.sleep(2500L);
@@ -23,12 +34,53 @@ public class HotSequenceTest {
 
         }
 
-        hotFlux.subscribe(singer -> System.out.println("# subscribe2 : " + singer + " Thread : " + Thread.currentThread()));
+        hotFlux.subscribe(singer -> log.warn(("# subscribe2 : " + singer)));
 
         try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
 
         }
+    }
+
+    @Test
+    void HTTP_Request_Hot_Sequence() {
+        final URI uri = UriComponentsBuilder.newInstance().scheme("http")
+                .host("worldtimeapi.org")
+                .port(80)
+                .path("/api/timezone/Asia/Seoul")
+                .build()
+                .encode()
+                .toUri();
+
+        final Mono<String> mono = getWorldTime(uri)
+                .cache();
+        mono.subscribe(dateTime -> log.info("# dateTime 1: {}", dateTime));
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        mono.subscribe(dateTime -> log.info("# dateTime 2: {}", dateTime));
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Mono<String> getWorldTime(final URI uri) {
+        return WebClient.create()
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(response -> {
+                    final DocumentContext jsonContext = JsonPath.parse(response);
+                    return jsonContext.read("$.datetime");
+                });
     }
 }
